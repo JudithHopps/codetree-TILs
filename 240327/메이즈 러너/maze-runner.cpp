f813b9e660660a51005c0967b04c61a32dc88dfa
subin <113181934+JudithHopps@users.noreply.github.com>
2024-03-27 15:56:55 +0900

    Title: 메이즈 러너, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240327/메이즈 러너/maze-runner.cpp
+++ 240327/메이즈 러너/maze-runner.cpp
@@ -1,242 +1,211 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cstring>
 
 using namespace std;
-int n, m, k, a[14][14],b[14][14],c[14][14], d, EY, EX,y,x,py[14],px[14], ret;
-struct A {
-    int dist, y, x;
-};
-vector<A> people;
 
-bool cmp (A c , A b){
-    if(c.dist==b.dist){
-            if(c.y==b.y){
-                return c.x < b.x;
-            }
-            return c.y < b.y;
-    }
-    return c.dist < b.dist;
-    
-}
-void print() {
-    cout << "\n";
-    for (int i = 0; i < n;i++){
-        for (int j = 0; j < n;j++){
-            if(i==EY && j==EX){
-                cout << "E ";
-            }else {
+#define MAX_N 10
 
-            cout << a[i][j] << " ";
+int n, m, k;
+
+// 모든 벽들의 상태를 기록해줍니다.
+int board[MAX_N + 1][MAX_N + 1];
+
+// 회전의 구현을 편하게 하기 위해 2차원 배열을 하나 더 정의해줍니다.
+int next_board[MAX_N + 1][MAX_N + 1];
+
+// 참가자의 위치 정보를 기록해줍니다.
+pair<int, int> traveler[MAX_N + 1];
+
+// 출구의 위치 정보를 기록해줍니다.
+pair<int, int> exits;
+
+// 정답(모든 참가자들의 이동 거리 합)을 기록해줍니다.
+int ans;
+
+// 회전해야 하는 최소 정사각형을 찾아 기록해줍니다.
+int sx, sy, square_size;
+
+// 모든 참가자를 이동시킵니다.
+void MoveAllTraveler() {
+    // m명의 모든 참가자들에 대해 이동을 진행합니다.
+    for(int i = 1; i <= m; i++) {
+        // 이미 출구에 있는 경우 스킵합니다.
+        if(traveler[i].first == exits.first && traveler[i].second == exits.second)
+            continue;
+        
+        // 행이 다른 경우 행을 이동시켜봅니다.
+        if(traveler[i].first != exits.first) {
+            int ny = traveler[i].first;
+            int nx = traveler[i].second;
+
+            if(exits.first > ny) ny++;
+            else ny--;
+
+            // 벽이 없다면 행을 이동시킬 수 있습니다.
+            // 이 경우 행을 이동시키고 바로 다음 참가자로 넘어갑니다.
+            if(!board[ny][nx]) {
+                traveler[i].first = ny;
+                traveler[i].second = nx;
+                ans++;
+                continue;
             }
         }
-        cout << "\n";
+
+        // 열이 다른 경우 열을 이동시켜봅니다.
+        if(traveler[i].second != exits.second) {
+            int ny = traveler[i].first;
+            int nx = traveler[i].second;
+
+            if(exits.second > nx) nx++;
+            else nx--;
+
+            // 벽이 없다면 행을 이동시킬 수 있습니다.
+            // 이 경우 열을 이동시킵니다.
+            if(!board[ny][nx]) {
+                traveler[i].first = ny;
+                traveler[i].second = nx;
+                ans++;
+                continue;
+            }
+        }
     }
 }
-void input() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
 
+// 한 명 이상의 참가자와 출구를 포함한 가장 작은 정사각형을 찾습니다.
+void FindMinimumSquare() {
+    // 가장 작은 정사각형부터 모든 정사각형을 만들어봅니다.
+    for(int sz = 2; sz <= n; sz++) {
+        // 가장 좌상단 r 좌표가 작은 것부터 하나씩 만들어봅니다.
+        for(int y1 = 1; y1 <= n - sz + 1; y1++) {
+            // 가장 좌상단 c 좌표가 작은 것부터 하나씩 만들어봅니다.
+            for(int x1 = 1; x1 <= n - sz + 1; x1++) {
+                int y2 = y1 + sz - 1;
+                int x2 = x1 + sz - 1;
+
+                // 만약 출구가 해당 정사각형 안에 없다면 스킵합니다.
+                if(!(y1 <= exits.first && exits.first <= y2 && x1 <= exits.second && exits.second <= x2)) {
+                    continue;
+                }
+
+                // 한 명 이상의 참가자가 해당 정사각형 안에 있는지 판단합니다.
+                bool is_traveler_in = false;
+                for(int l = 1; l <= m; l++) {
+                    if(y1 <= traveler[l].first && traveler[l].first <= y2 && x1 <= traveler[l].second && traveler[l].second <= x2) {
+                        // 출구에 있는 참가자는 제외합니다.
+                        if(!(traveler[l].first == exits.first && traveler[l].second == exits.second)) {
+                            is_traveler_in = true;
+                        }
+                    }
+                }
+
+                // 만약 한 명 이상의 참가자가 해당 정사각형 안에 있다면
+                // sx, sy, sqaure_size 정보를 갱신하고 종료합니다.
+                if(is_traveler_in) {
+                    sy = y1;
+                    sx = x1;
+                    square_size = sz;
+
+                    return;
+                }
+            }
+        }
+    }
+}
+
+// 정사각형 내부의 벽을 회전시킵니다.
+void RotateSquare() {
+    // 우선 정사각형 안에 있는 벽들을 1 감소시킵니다.
+    for(int y = sy; y < sy + square_size; y++) {
+        for(int x = sx; x < sx + square_size; x++)
+            if(board[y][x]) board[y][x]--;
+        }
+
+    // 정사각형을 시계방향으로 90' 회전합니다.
+    for(int y = sy; y < sy + square_size; y++) {
+        for(int x = sx; x < sx + square_size; x++){
+            // Step 1. (sx, sy)를 (0, 0)으로 옮겨주는 변환을 진행합니다. 
+            int ox = x - sx, oy = y - sy;
+            // Step 2. 변환된 상태에서는 회전 이후의 좌표가 (x, y) -> (y, square_n - x - 1)가 됩니다.
+            int ry = ox, rx = square_size - oy - 1;
+            // Step 3. 다시 (sx, sy)를 더해
+            next_board[ry + sy][rx + sx] = board[y][x];
+        }
+    }
+
+    // next_board 값을 현재 board에 옮겨줍니다.
+    for(int y = sy; y < sy + square_size; y++) {
+        for(int x = sx; x < sx + square_size; x++)
+            board[y][x] = next_board[y][x];
+        }
+}
+
+// 모든 참가자들 및 출구를 회전시킵니다.
+void RotateTravelerAndExit() {
+    // m명의 참가자들을 모두 확인합니다.
+    for(int i = 1; i <= m; i++) {
+        int y = traveler[i].first;
+        int x = traveler[i].second;
+        // 해당 참가자가 정사각형 안에 포함되어 있을 때에만 회전시킵니다.
+       if(sy <= y && y < sy + square_size && sx <= x && x < sx + square_size) {
+            // Step 1. (sx, sy)를 (0, 0)으로 옮겨주는 변환을 진행합니다. 
+            int oy = y - sy, ox = x - sx;
+            // Step 2. 변환된 상태에서는 회전 이후의 좌표가 (x, y) -> (y, square_n - x - 1)가 됩니다.
+            int rx = square_size - oy - 1, ry = ox;
+            // Step 3. 다시 (sx, sy)를 더해줍니다.
+            traveler[i] = make_pair(ry + sy, rx + sx);
+        }
+    }
+
+    // 출구에도 회전을 진행합니다.
+    int y = exits.first;
+    int x = exits.second;
+     if(sy <= y && y < sy + square_size && sx <= x && x < sx + square_size) {
+        // Step 1. (sx, sy)를 (0, 0)으로 옮겨주는 변환을 진행합니다. 
+        int oy = y - sy, ox = x - sx;
+        // Step 2. 변환된 상태에서는 회전 이후의 좌표가 (x, y) -> (y, square_n - x - 1)가 됩니다.
+        int rx = square_size - oy - 1, ry = ox;
+        // Step 3. 다시 (sx, sy)를 더해줍니다.
+        exits = make_pair(ry + sy, rx + sx);
+    }
+}
+
+int main() {
     cin >> n >> m >> k;
-    for (int i = 0; i < n;i++){
-        for (int j = 0; j < n;j++){
-            cin >> a[i][j];
-        }
-    }
-    for (int i = 0; i < m;i++){
-        cin >> py[i] >> px[i];
-        py[i]--, px[i]--;
-    }
-    cin >> EY >> EX;
-    EY--, EX--;
-    a[EY][EX] = 0;
-    for (int i = 0; i < m;i++){
-        int dist = abs(py[i]-EY) + abs(px[i]-EX);
-        // pq.push({dist, py[i], px[i]});
-        people.push_back({dist, py[i], px[i]});
+    for(int i = 1; i <= n; i++)
+        for(int j = 1; j <= n; j++)
+            cin >> board[i][j];
+    
+    for(int i = 1; i <= m; i++) {
+        cin >> traveler[i].first;
+        cin >> traveler[i].second;
     }
 
-}
-bool canGo(int ny,int nx,int i){
-    if(a[ny][nx] || ny<0||nx<0||ny>=n||nx>=n)
-        return false;
-    // cout << people[i].y <<" , " << people[i].x  << " 움직임\n" ;
-   
-    ret++;
-    return true;
-}
-void move (){
-    for (int i = 0; i < people.size();i++)
-    {
-        int y = people[i].y;
-        int x = people[i].x;
-        int dist = abs(EX -x) + abs(EY - y);
-        people[i].dist = dist;
-        int ny = y, nx = x;
+    cin >> exits.first;
+    cin >> exits.second;
 
-        if(people[i].y < EY){
-            ny = y + 1;
-            if(canGo(ny,nx,i)){
-                people[i] = {dist - 1, ny, nx};
-            }
-        } else if(people[i].y > EY){
-            ny = y - 1;
-            if(canGo(ny,nx,i)){
-                people[i] = {dist - 1, ny, nx};
-            }
-        }else if(people[i].x < EX){
-            nx = x + 1;
-            if(canGo(ny,nx,i)){
-                people[i] = {dist - 1, ny, nx};
-            }
-        }else if(people[i].x > EX){
-            nx = x - 1;
-            if(canGo(ny,nx,i)){
-                people[i] = {dist - 1, ny, nx};
+    while(k--) {
+        // 모든 참가자를 이동시킵니다.
+        MoveAllTraveler();
+
+        // 모든 사람이 출구로 탈출했는지 판단합니다.
+        bool is_all_escaped = true;
+        for(int i = 1; i <= m; i++) {
+            if(!(traveler[i].first == exits.first && traveler[i].second == exits.second)) {
+                is_all_escaped = false;
             }
         }
 
-        // 탈출
-        if(people[i].dist <= 0){
-            people.erase(people.begin() + i);
-            i--;
-        }
-    }
-}
-void rotSet90(int mnY, int mnX, int mxY, int mxX) {
-    int n = mxY - mnY + 1; // 세로 방향으로의 길이
-    // 가로 방향으로의 길이는 자동으로 처리됨 (n을 사용하여 정사각형 영역으로 가정)
-    for (int i = 0; i < n / 2; i++) {
-        for (int j = i; j < n - i - 1; j++) {
-            int temp = a[mnY + i][mnX + j]; // 상단 왼쪽 값을 임시 저장
+        // 만약 모든 사람이 출구로 탈출했으면 바로 종료합니다.
+        if(is_all_escaped) break;
 
-            // 시계 방향으로 90도 회전
-            a[mnY + i][mnX + j] = max(a[mxY - j][mnX + i] -1 , 0); // 하단 왼쪽 -> 상단 왼쪽
-            a[mxY - j][mnX + i] = max(a[mxY - i][mxX - j]-1, 0); // 하단 오른쪽 -> 하단 왼쪽
-            a[mxY - i][mxX - j] = max(a[mnY + j][mxX - i]-1, 0); // 상단 오른쪽 -> 하단 오른쪽
-            a[mnY + j][mxX - i] = max(temp-1 , 0); // 임시 저장한 값 -> 상단 오른쪽
-        }
+        // 한 명 이상의 참가자와 출구를 포함한 가장 작은 정사각형을 찾습니다.
+        FindMinimumSquare();
+
+        // 정사각형 내부의 벽을 회전시킵니다.
+        RotateSquare();
+        // 모든 참가자들 및 출구를 회전시킵니다.
+        RotateTravelerAndExit();
     }
 
-    a[mnY + n / 2][mnX + n / 2] = max(a[mnY + n / 2][mnX + n / 2]-1,0);
-}
-
-void rotate90(){
-    sort(people.begin(), people.end(), cmp);
-    // cout << "가까운 사람 : " << people[0].y << " , " << people[0].x << "\n";
-    // cout <<"출구        : " << EY << " , " << EX << "\n";
-    int mnY = min(people[0].y, EY);
-    int mnX = min(people[0].x, EX);
-    int mxY = max(people[0].y, EY);
-    int mxX = max(people[0].x, EX);
-
-    int diffY = mxY - mnY;
-    int diffX = mxX - mnX;
-    // cout <<"diff : " << diffY <<" , " <<  diffX << "\n";
-
-    if(diffY < diffX){
-        // y 좌표의 크기를 늘려야 함!! 
-        while(diffY != diffX){
-            mnY--;
-            diffY++;
-        }
-    } else if (diffX < diffY){
-        // x 좌표의 크기를 늘려야 함!!
-         while(diffY != diffX){
-            mnX--;
-            diffX++;
-        }
-    }
-    while(mnY<0){
-        mnY++;
-        mxY++;
-    }
-    while(mnX < 0){
-        mnX++;
-        mxX++;
-    }
-    // cout << mnY << " , " << mnX << "\n";
-    // cout << mxY << " , " << mxX << "\n";
-    // print();
-    rotSet90(mnY, mnX, mxY, mxX);
-
-    //     cout << mnY << " , " << mnY + siz << "\n";
-    //     cout << mn << " , " << mn + siz << "\n";
-    //     // cout << nx << ", " << mx << "\n";
-
-        // // 맵
-        // // mn ~ mx 정사각형 만큼 a를 시계방향으로 회전해줘
-
-        // // 배열 b에 배열 a를 시계방향으로 회전하여 저장
-        // //b[j][mx - i + mn] = a[i][j];!!!!
-        // for (int i = mnY; i <= mxY; i++){
-        //     for (int j = mnX; j <= mxX; j++){
-        //         b[x-mnX + mnY][mnY-y+mnX] = max(a[i][j]-1,0);
-        //     }
-        // }
-
-        // // 배열 b를 배열 a에 복사
-        // for (int i = mnY; i <= mxY; i++){
-        //     for (int j = mnX; j <= mxX; j++){
-        //         a[i][j] = b[i][j];
-        //     }
-        // }
-
-      // 사람 위치 업데이트
-    for (int i = 0; i < people.size(); i++) {
-        int y = people[i].y;
-        int x = people[i].x;
-        if (mxY >= y && y >= mnY && mxX >= x && x >= mnX) {
-           
-            people[i].y = mnY + (x - mnX); // 수정된 부분
-            people[i].x = mxX - (y - mnY); // 수정된 부분
-
-            if (people[i].y < mnY || people[i].y > mxY || people[i].x< mnX || people[i].x > mxX) {
-            // 탈출구의 새 위치가 사각형 영역을 벗어나는 경우, 기존 위치로 되돌림
-                people[i].y = y;
-                people[i].x = mnY + (mxX - y);
-            }
-        }
-    }
-
-    // 탈출구 위치 업데이트
-    // cout <<"탈출구 : " << EY << " , " << EX <<"\n";
-    if (mxY >= EY && EY >= mnY && mxX >= EX && EX >= mnX) {
-       int tempY = EY; // 임시 변수를 사용해 EY 값을 보존
-        EY = mnY + (EX - mnX); // 수정된 부분
-        EX = mxX - (tempY - mnY); // 수정된 부분
-
-        // 탈출구의 새 위치가 사각형 영역을 벗어나는지 확인하고 필요한 경우 조정
-        if (EY < mnY || EY > mxY || EX < mnX || EX > mxX) {
-            // 탈출구의 새 위치가 사각형 영역을 벗어나는 경우, 기존 위치로 되돌림
-            EY = tempY;
-            EX = mnY + (mxX - tempY);
-        }
-
-    }
-}
-
-
-int main(){
-
-    input(); 
-    // print(); 
-    while(k-- && !people.empty()){
-        move();
-        // print();
-        rotate90();
-
-        // print(); 
-
-        // cout << ret << "\n"
-        //  << EY+1 << " " << EX+1 << "\n";
-        // cout << "--------------------\n\n";
-    }
-
-    cout << ret << "\n"
-         << EY+1 << " " << EX+1 << "\n";
-
-    return 0;
+    cout << ans << "\n";
+    cout << exits.first << " " << exits.second;
 }
